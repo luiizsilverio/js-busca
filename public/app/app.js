201afdd1ca095361cca2a820453a693fe616ef63
luiizsilverio <luiiz.silverio@gmail.com>
2023-04-13 20:38:02 -0300

    módulo 4 ok

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,20 @@
 import { notasService as service } from "./nota/service.js";
+import { takeUntil, debounce } from "./utils/operators.js";
 import "./utils/array-helpers.js";
 
 const ehDivisivel = (divisor, numero) => !(numero % divisor);
 const ehDivisivelPorDois = ehDivisivel.bind(null, 2);
 
-
-document
-  .querySelector('#myButton')
-  .onclick = () => service
+const action = debounce(500, 
+  takeUntil(3, () =>
+    service
     .sumItems('2143')
     .then(console.log)
     .catch(console.warn)
+  )
+);
 
+document
+  .querySelector('#myButton')
+  .onclick = action
+;
